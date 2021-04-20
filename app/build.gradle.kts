b8892f05f4943a5fc29c9d7045ea442670536907
Raheem <raheemideadev@gmail.com>
2021-04-20 17:30:46 +0500

    resources

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,12 +23,18 @@ android {
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        compose = true
     }
 }
 
@@ -38,7 +44,36 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-    testImplementation("junit:junit:4.+")
+    testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+
+    // compose
+    implementation("androidx.compose.ui:ui:1.0.0-beta04")
+
+    // tooling support (previews, etc.)
+    implementation("androidx.compose.ui:ui-tooling:1.0.0-beta04")
+
+    // foundation (border, background, box, image, scroll, shapes, animations, etc.)
+    implementation("androidx.compose.foundation:foundation:1.0.0-beta04")
+
+    // material design
+    implementation("androidx.compose.material:material:1.0.0-beta04")
+
+    // material design icons
+    implementation("androidx.compose.material:material-icons-core:1.0.0-beta04")
+    implementation("androidx.compose.material:material-icons-extended:1.0.0-beta04")
+
+    // integration with activities
+    implementation("androidx.activity:activity-compose:1.3.0-alpha06")
+
+    // integration with viewmodel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha04")
+
+    // integration with observables
+    implementation("androidx.compose.runtime:runtime-livedata:1.0.0-beta04")
+    implementation("androidx.compose.runtime:runtime-rxjava2:1.0.0-beta04")
+
+    // animation
+    implementation("androidx.compose.animation:animation:1.0.0-beta04")
 }
